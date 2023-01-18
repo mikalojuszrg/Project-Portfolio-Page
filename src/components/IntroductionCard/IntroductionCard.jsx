8765ee93fcb4d2e37f85ce0a8c79ee8ebafea1d7
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-01-18 23:18:47 +0200

    applied some media-queries with mixins

--- src/components/IntroductionCard/IntroductionCard.jsx
+++ src/components/IntroductionCard/IntroductionCard.jsx
@@ -1,6 +1,5 @@
 import styles from "./IntroductionCard.module.scss";
 import { buttons } from "../../consts/buttons";
-
 import Button from "../Button/Button";
 
 const IntroductionCard = () => {
@@ -10,7 +9,10 @@ const IntroductionCard = () => {
       <p>I care about this and that.</p>
       <div className={styles.card__buttons}>
         {buttons.map((button) => (
-          <Button key={button.id} variant={button.variant}>
+          <Button
+            key={button.id}
+            // className={`${button.variant === "circle" && styles.card__circle}`}
+          >
             {button.title}
           </Button>
         ))}
